6eb7a96d8058633136ddc3da83113b93130bc827
charangowdabk11 <charangowdabk1103@gmail.com>
2026-06-16 08:49:09 +0530

    Update settings.py

--- votes/voting_system/settings.py
+++ votes/voting_system/settings.py
@@ -53,11 +53,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'voting_system.wsgi.application'
 
 # DATABASE
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    )
 }
 
 # PASSWORD VALIDATION
